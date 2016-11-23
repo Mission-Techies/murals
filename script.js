1343215d493ee9170210802efde09642e316d4d6
Abraham Velazquez Tello <abraham@lunaroja.net>
2016-11-23 21:41:07 +0000

    got detail mural id working and added more styles

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 var api_key = 'key2m8VgwGT2iztad';
 var all_murals= 'https://api.airtable.com/v0/appC2gzawbLgSvOeE/Art?api_key=' + api_key;
 
-var one_mural = 'https://api.airtable.com/v0/appC2gzawbLgSvOeE/Art/recpii7WnWlZ8sIPY?api_key=' + api_key;
+
+$.urlParam = function(name){
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	if (results) {
+	  return results[1] || 0;
+	}
+	return false;
+}
+
+var url_one_mural = function() {
+  var url_id = $.urlParam('muralID');
+  return 'https://api.airtable.com/v0/appC2gzawbLgSvOeE/Art/' + url_id +'?api_key=' + api_key;
+}
 
 // All details
 function renderRecords(data) {
@@ -12,15 +24,15 @@ function renderRecords(data) {
       var mural_pics = mural.fields['Pic'];
       var mural_info = '';
       if (mural_name) {
-        mural_info +=`<div class="panel panel-default">`;
-          mural_info +=`<div class="panel-body">`;
+        mural_info +=`<div class="media">`;
+          mural_info +=`<div class="media-left">`;
           if (mural_pics) {
             $.each(mural_pics, function(i, pic){
               mural_info +=`<a href="detail.html?muralID=${mural.id}"><img src="${pic.url}"></a>`;
             });
           }
           mural_info +=`</div>`;
-        mural_info += `<div class="panel-footer">${mural_name}</div>`;
+        mural_info += `<div class="media-body">${mural_name}</div>`;
         mural_info +=`</div>`;
       }
       $('.murals').append(mural_info);
@@ -46,10 +58,13 @@ function renderOneMural(mural) {
             });
           }
           mural_info +=`</div>`;
-        mural_info += `<div class="panel-footer">${mural_name}</div>`;
+        mural_info += `<div class="panel-footer">Name: ${mural_name}<br>Location: ${mural_where}</div>`;
         mural_info +=`</div>`;
       }
       $('.mural-detail').append(mural_info);
 }
 
-$.get(one_mural, renderOneMural);
+if ($.urlParam('muralID')) {
+  $.get(url_one_mural(), renderOneMural);   
+}
+
